b3e570eda3ae06852bfe5940444ccd3f785db4b4
Jon Jaffe <jonjaffe5@gmail.com>
2025-06-27 15:21:36 -0400

    Revert "Add BreakdownQuoteCard component and update quotes"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -92,16 +92,6 @@ export default {
           "Arial",
           "sans-serif",
         ],
-        poppins: [
-          "Poppins",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          "Segoe UI",
-          "Roboto",
-          "Helvetica Neue",
-          "Arial",
-          "sans-serif",
-        ],
       },
       keyframes: {
         "accordion-down": {
